6e9530066e78c5d056d19aa04b3a4103c4f96813
Diseyi Diffa <d.diffa@allegrodevelopment.com>
2018-08-29 10:42:56 -0500

    commit

--- EyeAppoint/EyeAppoint/Views/MainPage.xaml.cs
+++ EyeAppoint/EyeAppoint/Views/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
-
+﻿using Newtonsoft.Json;
+using System;
+using System.Data;
+using System.Net.Http;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,6 +12,13 @@ namespace EyeAppoint.Views
     {
         public MainPage()
         {
+            string Url = "https://cyclopsodyssey.com/pwa/server/server.php?action=getContactLens&fkcompanyid=95";
+            Title = "EyeAppoint";
+            var client = new HttpClient();
+            var json =  client.GetAsync(Url);
+            var responseJson = json.Result.Content.ReadAsStringAsync().Result;
+          //  var msg -JsonConvert.DeserializeObject<DataTable>(responseJson);
+         //   JsonConvert.DeserializeObject<ListView>(json.ToString());
             InitializeComponent();
         }
     }
