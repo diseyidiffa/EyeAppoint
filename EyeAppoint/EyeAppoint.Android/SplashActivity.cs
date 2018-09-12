c2c4c8ccf36eaa039a55f9ee3f7bd319f1927f9d
dizzydiffi <ddiffa@visionsoftwareonline.com>
2018-09-12 11:15:16 -0500

    splash screen

--- EyeAppoint/EyeAppoint.Android/SplashActivity.cs
+++ EyeAppoint/EyeAppoint.Android/SplashActivity.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
 namespace EyeAppoint.Droid
 {
-    public class SplashActivity
+    [Activity(Label = "Eye Appoint",
+              Icon = "@drawable/icon",
+              Theme = "@style/Theme.Splash", //Indicates the theme to use for this activity
+              MainLauncher = true, //Set it as boot activity
+              NoHistory = true)] //Doesn't place it in back stack
+    public class SplashActivity : Activity
     {
-        public SplashActivity()
+        protected override void OnCreate(Bundle savedInstanceState)
         {
+            base.OnCreate(savedInstanceState);
+
+            System.Threading.Thread.Sleep(3000); //Let's wait awhile...
+            this.StartActivity(typeof(MainActivity));
         }
     }
 }
