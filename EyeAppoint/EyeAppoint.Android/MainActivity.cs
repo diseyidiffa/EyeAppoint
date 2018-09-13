68988a411c89ea7f1a236087736f9f605cb29958
Diseyi Diffa <d.diffa@allegrodevelopment.com>
2018-09-13 13:07:03 -0500

    android changes

--- EyeAppoint/EyeAppoint.Android/MainActivity.cs
+++ EyeAppoint/EyeAppoint.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace EyeAppoint.Droid
 {
-    [Activity(Label = "EyeAppoint", Icon = "@drawable/icon", Theme = "@style/MyTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+    [Activity(Label = "EyeAppoint", Icon = "@drawable/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
      public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ EyeAppoint/EyeAppoint/Views/NoInternetPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace EyeAppoint.Views
+{
+	public class NoInternetPage : ContentPage
+	{
+        Image splashImage;
+
+        public NoInternetPage()
+		{
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            var sub = new AbsoluteLayout();
+            splashImage = new Image
+            {
+                Source = "icon.png",
+                WidthRequest = 100,
+                HeightRequest = 100
+            };
+            AbsoluteLayout.SetLayoutFlags(splashImage,
+               AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(splashImage,
+             new Rectangle(0.5, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            sub.Children.Add(splashImage);
+
+            this.BackgroundColor = Color.FromHex("#429de3");
+            this.Content = sub;
+        }
+
+    }
+}
